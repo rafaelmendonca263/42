b5e701798c0276e23f07bbf1667ce363b818ad5a
Rafael Mendonça <rafaelmendonca263@gmail.com>
2025-10-26 01:05:16 +0100

    update

--- Libft/libft.h
+++ Libft/libft.h
@@ -60,7 +60,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 
-// Bónus
+// Bónus (lists)
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
@@ -70,4 +70,11 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+// Struct
+typedef struct s_list
+{
+    void *content;
+    struct s_list *next;
+} t_list;
 #endif
